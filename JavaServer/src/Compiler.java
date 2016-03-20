249769a460716fefb264cf1dcc7cc3504b9f2d5e
Vladimir Karassouloff <vkarassouloff@hotmail.fr>
2016-03-20 15:10:29 +0100

    Bla bla bla

--- JavaServer/src/Compiler.java
+++ JavaServer/src/Compiler.java
@@ -7,7 +7,7 @@ public class Compiler {
 
 
     public static String compileCode(String code){
-        System.out.println("On a bien recu le code "+code);
+        System.out.println("On a bien recu le code :\n"+code);
         return "Succès, debut execution";
     }
 
