70b78c23ecba975b257056e663d1445c799d887a
juliengauttier <julien.gauttier@gmail.com>
2016-05-19 15:30:35 +0200

    new menu

---
+++ Android/app/src/main/java/com/example/vladimirkarassouloff/projetter/ui/MenuActivity.java
@@ -0,0 +1,16 @@
+package com.example.vladimirkarassouloff.projetter.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.vladimirkarassouloff.projetter.R;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+    }
+}
